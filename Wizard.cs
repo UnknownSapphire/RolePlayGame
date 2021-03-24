776d9e254636b13f2c49916e8a85070405971acc
PongLenis <timur322132@mail.ru>
2021-03-24 21:36:55 +0300

    add character and wizard class files

---
+++ Wizard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Character {
+    public enum Spell {
+        
+    }
+
+    public enum PowerSpell {
+        Heal
+    }
+    
+    public class Wizard: Character {
+        public Wizard(string name, Gender gender, Race race, int age): base(name, gender, race, age) {}
+
+        public int Mana {
+            get => _mana;
+            private set => _mana = Math.Max(0, Math.Min(value, MaxMana));
+        }
+
+        public void SpellCast(PowerSpell spell, Character other, int power) {
+            switch (spell) {
+                case PowerSpell.Heal:
+                    if (other.State != State.Dead) {
+                        int hp = Math.Min(power, Math.Min(Mana / 2, MaxHealth - other.Health));
+                        other.Health += hp;
+                        Mana -= hp * 2;
+                    }
+                    break;
+            }
+        }
+
+        public void SpellCast(Spell spell, Character other) {
+            if ((int) spell <= Mana) {
+                Mana -= (int) spell;
+            }
+        }
+
+        public override object Clone() {
+            Wizard wizard = this;
+            wizard._id = _nextId++;
+            return wizard;
+        }
+
+        public override string ToString() {
+            return base.ToString() + ", Mana: " + Mana;
+        }
+
+        public const int MaxMana = 100;
+        private int _mana = MaxMana;
+    }
+}
